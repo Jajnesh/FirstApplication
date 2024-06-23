7bdf060b165537f241377463e6fd3b183f7f7b29
Jajnesh <jajneshbehera2002@gmail.com>
2024-06-23 19:25:15 +0530

    Implement GetProductDetails

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,5 +68,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.material3:material3")
     implementation ("androidx.compose.material:material-icons-extended")
-
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,250 +1,172 @@
 package com.example.firstapplication
 
-import android.annotation.SuppressLint
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.basicMarquee
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material.icons.sharp.CheckCircle
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.AssistChipDefaults
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ElevatedSuggestionChip
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.ProgressIndicatorDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.ScaffoldDefaults
-import androidx.compose.material3.ShapeDefaults
-import androidx.compose.material3.Slider
-import androidx.compose.material3.SuggestionChip
-import androidx.compose.material3.Switch
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.example.firstapplication.ui.theme.FirstApplicationTheme
+import androidx.lifecycle.viewModelScope
+import coil.compose.AsyncImage
+import kotlinx.coroutines.launch
+import org.jetbrains.annotations.Async
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalFoundationApi::class)
+    private val productVM: ProductViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
         setContent {
-            Column (modifier = Modifier.padding(25.dp)) {
-                ElevatedCard(
-                    modifier = Modifier
-                        .height(500.dp)
-                        .fillMaxWidth(),
-                    colors = CardDefaults.cardColors(Color(235, 238, 255))
+            val products by productVM.products.observeAsState(emptyList())
+            Surface(modifier = Modifier.fillMaxSize().padding(5.dp), color = MaterialTheme.colorScheme.background) {
+                LazyVerticalStaggeredGrid(
+                    columns = StaggeredGridCells.Fixed(2)
                 ) {
-                    Column (
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = "Jetpack Compose",
-                            color = Color(22, 78, 120),
-                            modifier = Modifier.padding(top = 20.dp),
-                            fontSize = 27.sp
-                        )
-                        Image(
-                            painter = painterResource(id = R.drawable.jetpackicon),
-                            contentDescription = "Jetpack_Logo",
-                            modifier = Modifier.size(100.dp),
-                            contentScale = ContentScale.Fit
-                        )
-                        Text(
-                            text = "Login",
-                            color = Color(46, 115, 57),
-                            modifier = Modifier
-                                .padding(20.dp)
-                                .fillMaxWidth(),
-                            fontSize = 32.sp,
-                            textAlign = TextAlign.Start
-                        )
-
-                        var username by remember {
-                            mutableStateOf("")
-                        }
-                        OutlinedTextField(
-                            value = username,
-                            onValueChange = { username = it },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 15.dp),
-                            label = { Text("Email ID or Mobile Number") },
-                            maxLines = 1
-                        )
-
-                        Spacer(modifier = Modifier.height(15.dp))
-
-                        var showPassword by remember {
-                            mutableStateOf(false)
-                        }
-                        var password by remember {
-                            mutableStateOf("")
-                        }
-                        val passwordVisualTransformation = remember { PasswordVisualTransformation() }
-                        OutlinedTextField(
-                            value = password,
-                            onValueChange = { password = it },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 15.dp),
-                            label = { Text(text = "Password")},
-                            maxLines = 1,
-                            visualTransformation = if (showPassword) {
-                                VisualTransformation.None
-                            } else {
-                                passwordVisualTransformation
-                            },
-                            trailingIcon = {
-                                Icon(
-                                    if (showPassword) {
-                                        Icons.Filled.Visibility
-                                    } else {
-                                        Icons.Filled.VisibilityOff
-                                    },
-                                    contentDescription = "Visibility_Icon",
-                                    modifier = Modifier.clickable { showPassword = !showPassword }
-                                )
-                            }
-                        )
-
-                        Text(
-                            text = "Forgot Password?",
-                            color = Color(46, 100, 57),
-                            fontSize = 15.sp,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(15.dp)
-                                .clickable {  },
-                            textAlign = TextAlign.End,
-                        )
-
-                        Row (modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 15.dp)){
-                            Button(
-                                onClick = { /*TODO*/ },
-                                colors = ButtonDefaults.buttonColors(Color(22, 78, 120)),
-                                modifier = Modifier
-                                    .width(120.dp)
-                                    .height(60.dp)
-                            ) {
-                                Text(text = "Login", fontSize = 18.sp)
-                            }
-                        }
+                    items(products) {product ->
+                        ProductItem(product = product)
                     }
                 }
+            }
+        }
+    }
 
-                Spacer(modifier = Modifier.height(20.dp))
+    @Composable
+    fun ProductItem(product: Product) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                AsyncImage(model= product.image, contentDescription = "Unable to load at the moment")
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = product.title, color = Color.Blue)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = product.category)
+            }
+        }
+    }
+}
 
-                Column (
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Row {
-                        Text(
-                            text = "Don't have an account?",
-                            fontSize = 16.sp
-                        )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text(
-                            text = "Register",
-                            color = Color(22, 78, 180),
-                            modifier = Modifier.clickable {},
-                            fontSize = 16.sp
-                        )
-                    }
-                }
+
+//Model Class
+data class  Product(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    val image: String,
+    val rating: Rating
+)
+
+data class Rating(
+    val rate: Float,
+    val count: Int
+)
+
+interface  ApiService {
+    @GET("products")
+    suspend fun getProducts(): List<Product>
+}
+
+object RetrofitClient {
+    private const val BASE_URL = "https://fakestoreapi.com/"
+
+    val  apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
+
+class ProductRepository(private val apiService: ApiService) {
+    suspend fun getProducts(): List<Product> {
+        return apiService.getProducts()
+    }
+}
+
+class ProductViewModel: ViewModel() {
+    private  val _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>> get() = _products
+
+    private  val repository = ProductRepository(RetrofitClient.apiService)
+    init {
+        fetchProducts()
+    }
+
+    fun fetchProducts() {
+        viewModelScope.launch {
+            try {
+                val productList = repository.getProducts()
+                _products.postValue(productList)
+                println(productList)
+
+            } catch (e: Exception) {
+
             }
         }
     }
